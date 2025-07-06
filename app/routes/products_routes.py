a2d2f07d8d6f02398c93f29014d2b0afa63843b4
vinikobs <viniciusrehbeinkobs@gmail.com>
2025-07-06 19:59:46 -0300

    add POST method to /products route and new /products/{id} GET route for product-specific data dispatch

--- app/routes/products_routes.py
+++ app/routes/products_routes.py
@@ -4,7 +4,13 @@ from ..http.dispatcher import dispatch
 from app.http.middleware import jwt_required
 products_route = Blueprint('products_route', __name__)
 
-@products_route.route('/products', methods=["GET"])
+@products_route.route('/products', methods=["GET", "POST"])
 @jwt_required
 def __products():
     if request.method == "GET": return dispatch(ProductsController.list)
+    if request.method == "POST": return dispatch(ProductsController.calc)
+
+@products_route.route('/products/{id}', methods=["GET"])
+@jwt_required
+def __product_id(_id):
+    if request.method == "GET": return dispatch(ProductsController.listById)
