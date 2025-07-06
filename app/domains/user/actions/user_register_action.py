8806d2a6ae2aca6bf64df1cbe4c0761544cf4b8d
Pedro <eduba.oscar@gmail.com>
2025-07-06 15:15:08 -0300

    f

--- app/domains/user/actions/user_register_action.py
+++ app/domains/user/actions/user_register_action.py
@@ -1,6 +1,9 @@
 from app.database.db import DB
 from app.models.user_model import UserModel
+from app.service.hash import hash_password
+
 
 def userRegisterAction(user):
+    user.password = hash_password(user.password)
     UserModel.register(user)
     DB.commit()
